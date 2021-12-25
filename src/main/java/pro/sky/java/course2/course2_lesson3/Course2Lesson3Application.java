55cab6793b66b2cc6fbce446f380e5695b274f78
Елена <etretjakowa@yandex.ru>
2021-12-25 14:54:19 +0700

    ДЗ

---
+++ src/main/java/pro/sky/java/course2/course2_lesson3/Course2Lesson3Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.course2_lesson3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2Lesson3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course2Lesson3Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/course2_lesson3/Course2Lesson3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.course2_lesson3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2Lesson3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
